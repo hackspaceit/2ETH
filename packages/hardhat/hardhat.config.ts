b992b480bbf2bfdec31066d23ba2bad490380e8f
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-12 01:00:01 +0700

    UPDATE

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -14,7 +14,7 @@ import "hardhat-deploy-ethers";
 // You can get your own at https://dashboard.alchemyapi.io
 const providerApiKey = "process.env.ALCHEMY_API_KEY";
 // If not set, it uses the hardhat account 0 private key.
-const deployerPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const deployerPrivateKey = "process.env.DEPLOYER_PRIVATE_KEY";
 // If not set, it uses ours Etherscan default API key.
 const basescanApiKey = "process.env.BASESCAN_API_KEY";
 
