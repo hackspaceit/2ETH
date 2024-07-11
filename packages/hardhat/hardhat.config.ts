73dc34cd46cc378b995cba4626f00f9f16383b6a
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-12 00:32:28 +0700

    update

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  defaultNetwork: "base",
+  defaultNetwork: "Base",
   namedAccounts: {
     deployer: {
       // By default, it will take the first Hardhat account as the deployer
